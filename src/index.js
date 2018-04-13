331082d4a8eef7c93e79867907c2ccc97018f444
Anup Shrestha <Anup.Shrestha@infokonn.com>
2018-04-13 15:04:52 +0545

    simple redux example

--- src/index.js
+++ src/index.js
@@ -4,5 +4,30 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+
+function reducer(state, action){
+    console.log(action);
+    if(action.type === 'changeState'){
+        return action.payload.newState;
+    }
+    return 'State';
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+const action = {
+    type: 'changeState',
+    payload: {
+        newState: 'New state'
+    }
+};
+
+store.dispatch(action);
+
+console.log(store.getState());
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
